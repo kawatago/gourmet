d1a31159db4abbec90b57bbf54edde3111534d32
Shingo <kawata.a0110@gmail.com>
2019-02-20 00:19:46 +0900

    chartjsの導入

--- frontend/src/components/main/Main.vue
+++ frontend/src/components/main/Main.vue
@@ -1,12 +1,52 @@
 <template>
     <div class="main">
         contents is here ...
+        <RadarChart/>
     </div>
 </template>
 
 <script>
+import { Radar } from 'vue-chartjs'
+
+var RadarChart = {
+	extends: Radar,
+	mounted () {
+		this.renderChart({
+			// ラベル
+			labels: ["ストーリー", "キャラクター", "音楽", "バトル"],
+			// データ詳細
+			datasets: [{
+					// データタイトル
+					label: 'アニメ1',
+					// データ
+					data: [5, 4, 1, 3],
+					backgroundColor: [
+						'rgba(100, 130, 255, 0.2)'						
+					]
+			},
+			{
+					// データタイトル
+					label: 'アニメ2',
+					// データ
+					data: [4, 5, 0, 2],
+					backgroundColor: [
+						'rgba(130, 255, 100, 0.2)'						
+					],
+					borderWidth: 6,
+					borderDash: [1,1],
+					pointHoverBorderWidth: 10
+			}
+			]
+		});
+	}
+};
+
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components: {
+		RadarChart
+	}
 }
 </script>
 
