38b4bc8be0fad793d2062b1df665f52ecc99e865
Shingo <kawata.a0110@gmail.com>
2019-02-25 11:36:56 +0900

    post 確認完了

--- frontend/src/components/main/Main.vue
+++ frontend/src/components/main/Main.vue
@@ -12,31 +12,32 @@ var RadarChart = {
 	extends: Radar,
 	mounted () {
 		this.renderChart({
-			// ラベル
-			labels: ["ストーリー", "キャラクター", "音楽", "バトル"],
+            // ラベル
+            // 味、値段、店の雰囲気、アクセス、お酒に合う
+			labels: ["味", "コストパフォーマンス", "雰囲気", "アクセス","飲み会？"],
 			// データ詳細
 			datasets: [{
 					// データタイトル
-					label: 'アニメ1',
+					label: 'content1',
 					// データ
-					data: [5, 4, 1, 3],
+					data: [5, 4, 1, 3, 3],
 					backgroundColor: [
 						'rgba(100, 130, 255, 0.2)'						
 					]
-			},
-			{
-					// データタイトル
-					label: 'アニメ2',
-					// データ
-					data: [4, 5, 0, 2],
-					backgroundColor: [
-						'rgba(130, 255, 100, 0.2)'						
-					],
-					borderWidth: 6,
-					borderDash: [1,1],
-					pointHoverBorderWidth: 10
-			}
-			]
+			// },
+			// {
+			// 		// データタイトル
+			// 		label: 'アニメ2',
+			// 		// データ
+			// 		data: [4, 5, 0, 2],
+			// 		backgroundColor: [
+			// 			'rgba(130, 255, 100, 0.2)'						
+			// 		],
+			// 		borderWidth: 6,
+			// 		borderDash: [1,1],
+			// 		pointHoverBorderWidth: 10
+			// }
+            }]
 		});
 	}
 };
